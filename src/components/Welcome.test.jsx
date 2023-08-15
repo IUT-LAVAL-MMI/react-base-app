61637a8fc82110935497e3f62bd2b5b509d6ed87
Rémi Venant <remi.venant@univ-lemans.fr>
2023-08-16 01:00:03 +0200

    Add simple component test

---
+++ src/components/Welcome.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Welcome from './Welcome';
+
+it('Le Welcome ne change pas', () => {
+  global.APP_ENV = {
+    APP_TITLE: 'appTitle',
+  };
+
+  const component = renderer.create(
+    <Welcome />,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
